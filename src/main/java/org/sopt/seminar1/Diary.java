3d3dd39942d19218cdc773de504beb3c47b66eb6
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-10-09 14:23:09 +0900

    [Feat]: Diary 객체 구현

--- src/main/java/org/sopt/seminar1/Diary.java
+++ src/main/java/org/sopt/seminar1/Diary.java
@@ -1,4 +1,20 @@
 package org.sopt.seminar1;
 
 public class Diary {
+
+    private long id;
+    private String content;
+
+    public Diary(final long id, final String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
