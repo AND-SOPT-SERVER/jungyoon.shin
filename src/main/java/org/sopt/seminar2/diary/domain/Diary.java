fc91200cc05575565dec200a54337d58083189a8
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-10-26 01:52:02 +0900

    [feat]: 일기 작성시 글자수 검증 구현
    - Validation 어노테이션을 통해, 일기 작성 시 글자 수를 30자 이하로 제한하도록 했습니다.

---
+++ src/main/java/org/sopt/seminar2/diary/domain/Diary.java
@@ -0,0 +1,46 @@
+package org.sopt.seminar2.diary.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    private Diary(final String title, final String content) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static Diary create(final String title, final String content) {
+        return Diary.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+
+}
