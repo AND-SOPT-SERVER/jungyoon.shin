ea2dcfefaa45f8d2e36967815dd045bfac034d31
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-11-01 02:21:48 +0900

    [feat]: 유효하지 않은 카테고리 값 검증 로직 추가

--- src/main/java/org/sopt/seminar2/diary/domain/enums/Category.java
+++ src/main/java/org/sopt/seminar2/diary/domain/enums/Category.java
@@ -1,8 +1,11 @@
 package org.sopt.seminar2.diary.domain.enums;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.seminar2.diary.common.exception.DiaryException;
 
 import java.util.Arrays;
+
+import static org.sopt.seminar2.diary.common.code.FailureCode.NOT_EXISTS_CATEGORY;
 
 @RequiredArgsConstructor
 public enum Category {
@@ -15,6 +18,7 @@ public enum Category {
     private final String name;
 
     public static Category findCategory(String name) {
-        Arrays.stream(Category.values()).filter(category -> category.name.equals(name)).findAny().orElseThrow()
+        return Arrays.stream(Category.values()).filter(category -> category.name.equals(name)).findAny()
+                .orElseThrow(() -> new DiaryException(NOT_EXISTS_CATEGORY));
     }
 }
